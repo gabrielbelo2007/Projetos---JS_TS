2ee3c7c2d13a361b63e7be7f60c5412592a99426
Gabriel <gabrielbelo2007@gmail.com>
2025-02-18 18:20:30 -0300

    Timer - Update

--- Timer/assets/js/main.js
+++ Timer/assets/js/main.js
@@ -1,40 +1,41 @@
-const timer = document.querySelector(".timer");
-const iniciar = document.querySelector("#start");
-const pausar = document.querySelector("#pause");
-const zerar = document.querySelector("#reset");
+function timer () {
+    const timer = document.querySelector(".timer");
 
-function getTime(segundos) {
-    const contarTempo = new Date(segundos * 1000);
-    return contarTempo.toLocaleTimeString('pt-BR', {
-        hour12:false,
-        timeZone: 'UTC',
+    function getTime(segundos) {
+        const contarTempo = new Date(segundos * 1000);
+        return contarTempo.toLocaleTimeString('pt-BR', {
+            hour12:false,
+            timeZone: 'UTC',
+        });
+    };
+
+    let sec = 0;
+    let contando;
+
+    function updateTime() {
+        contando = setInterval(function() {
+            sec++;
+            timer.innerHTML = `<p>${getTime(sec)}</p>`;
+        }, 1000);
+    };
+
+    document.addEventListener('click', function(event){
+        let idBotao = event.target;
+
+        if (idBotao.classList.contains('start')) {
+            clearInterval(contando);
+            timer.style.color = 'black';
+            updateTime();
+        } else if (idBotao.classList.contains('pause')) {
+            timer.style.color = 'red';
+            clearInterval(contando);
+        } else if (idBotao.classList.contains('reset')) {
+            clearInterval(contando);
+            timer.style.color = 'black';
+            timer.innerHTML = `<p>00:00:00</p>`;
+            sec = 0;
+        };
     });
 };
 
-let sec = 0;
-let contando;
-
-function updateTime() {
-    contando = setInterval(function() {
-        sec++;
-        timer.innerHTML = `<p>${getTime(sec)}</p>`;
-    }, 1000);
-};
-
-iniciar.addEventListener('click', function(event){
-    clearInterval(contando);
-    timer.style.color = 'black';
-    updateTime();
-});
-
-pausar.addEventListener('click', function(event){
-    timer.style.color = 'red';
-    clearInterval(contando);
-});
-
-reset.addEventListener('click', function(event){
-    clearInterval(contando);
-    timer.style.color = 'black';
-    timer.innerHTML = `<p>00:00:00</p>`;
-    sec = 0;
-});
+timer();
